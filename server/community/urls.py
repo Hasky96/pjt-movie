2ed849a25dbf2d10d4a474c3994f1a4131a8831c
손한기 <wwenxt@naver.com>
2021-11-18 10:59:09 +0900

    community/recommend update

--- server/community/urls.py
+++ server/community/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.community_main)
+    path('', views.community_main),
+    path('recommend/', views.movie_recommend),
 ]
