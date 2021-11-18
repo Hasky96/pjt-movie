884c698eb77e1be4a6f51435b4d62dbba5a06b75
Haseok Jang <haseok1020@naver.com>
2021-11-18 22:46:04 +0900

    commit

--- client/src/views/Movies.vue
+++ client/src/views/Movies.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="movies">
-    <movies-list
+    <div v-if=movie class="moviesChildModal">
+      <button @click="exitModal">exit</button>
+      <movies-list-item
+      :movie="movie"
+      ></movies-list-item>
+    </div>
+    <div>
+    <movies-list class="moviesChild"
+    @show-detail="showDetail"
     >
     </movies-list>
+    </div>
   </div>
 </template>
 
 <script>
 import MoviesList from '@/components/MoviesList.vue'
+import MoviesListItem from '@/components/MoviesListItem.vue'
 import {mapState} from 'vuex'
 
 
@@ -15,9 +25,19 @@ export default {
   name: 'Movies',
   components: {
     MoviesList,
+    MoviesListItem
   },
   data : function(){
     return {
+      movie:null
+    }
+  },
+  methods:{
+    showDetail(movie){
+      this.movie = movie
+    },
+    exitModal(){
+      this.movie = null
     }
   },
   computed:{
@@ -25,3 +45,16 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.moviesChild{
+  position: absolute;
+  z-index: 0;
+}
+
+.moviesChildModal{
+  position: absolute;
+  z-index: 1;
+  background: #eee;
+}
+</style>
