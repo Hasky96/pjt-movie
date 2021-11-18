ae9db7f1a129a6fb9e00c0d328a5e92409d99aa6
손한기 <wwenxt@naver.com>
2021-11-18 15:28:56 +0900

    recommend complete

--- server/community/views.py
+++ server/community/views.py
@@ -39,111 +39,47 @@ import pickle
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 
+# Word2Vec 모델 불러오기
+embedding_model = gensim.models.Word2Vec.load('community\word2VecModel')
+
+
 
 #--------- 페이지 정의 -----------#
 def community_main(request):
     pass
 
 ###------------ 추천시스템 -------------- ###
+@api_view(['POST'])
+@permission_classes([AllowAny])
 def movie_recommend(request):
-    # Word2Vec 모델 불러오기
-    embedding_model = gensim.models.Word2Vec.load('community\word2VecModel')
-    print(embedding_model)
-    # 시각화 함수 정의
-    def w2vvisualization(keyword):
-        while True:
-            try: 
-                labels = []
-                tokens = []
-                okt = Okt()
-
-                keyword2 = okt.morphs(keyword, stem=True)
-                FirstKeyword = keyword2[0]
-
-                VecWords = embedding_model.wv.most_similar(positive=[FirstKeyword], topn=30)
-                tokens = []
-                labels = []
-    #             print(VecWords)
-    #             print(embedding_model.get_item()) 
-                for i in VecWords :
-                    labels.append(i[0])
-                    tokens.append(embedding_model.wv[i[0]])
-
-                tsne_model = TSNE(perplexity=40, n_components=2, init='pca', n_iter=2500, random_state=23)
-                new_values = tsne_model.fit_transform(tokens)
-
-                x = []
-                y = []
-
-                for value in new_values:
-                    x.append(value[0])
-                    y.append(value[1])
-                # print(x, y)
-
-                a = pd.DataFrame(labels)
-                x = pd.DataFrame(x)
-                y = pd.DataFrame(y)
-
-                df = pd.concat([a, x, y], axis = 1)
-                df.loc[30] = (keyword, 0, 0)
-                df.columns  = ['title', 'x', 'y']
-                df
-
-                plt.figure(figsize=(16, 16)) 
-                plt.rc("font", size = 20)
-                for i in range(len(x)-1):
-                    # plt.rcParams['font.family'] = 'hanygo250'
-                    plt.rcParams['axes.unicode_minus'] = False
-                    a = df.loc[[i, 30], :]
-                    plt.plot(a.x,a.y, '-D', linewidth = 2)
-                    plt.annotate(labels[i],
-                                        xy=(df.x[i], df.y[i]),
-                                        xytext=(5, 2),
-                                        textcoords='offset points',
-                                        ha='right',
-                                        va='bottom')
-                
-                
-                
-                # plt.scatter(df.x[30], df.y[30], s = 1000, marker= '*')
-                plt.annotate(keyword,
-                                xy=(0, 0),
-                                xytext=(10, 5),
-                                textcoords='offset points',
-                                ha='right',
-                                va='bottom')
-
-
-                plt.show()
-                break
-
-            except KeyError: 
-                print('다시 입력해주세요. 키워드가 존재하지 않습니다. ')
-                pass
-
+    # 키워드 check (입력 키워드가 이상하면 다시)
     def keyword_check(keyword):
-        while True:
-            try:
-                keyword = okt.morphs(keyword, stem=True)
-                FirstKeyword = keyword[0]
-                # check embedding result
-                VecWords = embedding_model.wv.most_similar(positive=[FirstKeyword], topn=100)
-                Veclist1 = []
-                for i in range(0,31):
-                    Veclist1.append(VecWords[i][0])
-            except KeyError: 
-                print('다시 입력해주세요. 키워드가 존재하지 않습니다. ')
-                pass
-
-    word = input("키워드 입력 : ") #로맨틱 #아련한
-    keyword_check(word)
-
-
-
-
-
-
-
+        try:
+            keyword = okt.morphs(keyword, stem=True)
+            FirstKeyword = keyword[0]
+            # check embedding result
+            VecWords = embedding_model.wv.most_similar(positive=[FirstKeyword], topn=100)
+            Veclist1 = []
+            for i in range(0,31):
+                Veclist1.append(VecWords[i][0])
+            context = {
+                'state' : True,
+                'Veclist1' : Veclist1[:10],
+                'first_keyword' : keyword
+            }
+            return context
+        except KeyError: 
+            # keyError 값을 return 해줘야 하는데??
+            # 리턴값이 XX 이면 ALTER 을 발생?
+            context = {
+                'state' : False
+            }
+            return context
+    # 이때 키워드는 request 에 담겨서 넘어와야 한다.
+    word  = request.POST.get('name')#로맨틱 #아련한
+    print(word)
+    context = keyword_check(word)
+    return Response(context, status=status.HTTP_200_OK)
 
 
 
