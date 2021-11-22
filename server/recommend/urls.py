346c4f20e34a7c5bcfe223ec103d8116e6f1ee46
손한기 <wwenxt@naver.com>
2021-11-22 13:39:04 +0900

    title base recommend create

--- server/recommend/urls.py
+++ server/recommend/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.movie_recommend),
     path('movie/', views.show_recommend_movie),
+    path('title/', views.title_recommend),
+    path('catch/', views.catch_data),
 ]
